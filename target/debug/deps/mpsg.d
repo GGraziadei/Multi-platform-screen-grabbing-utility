9797b21ced0c5a8f6df5887b6e25e5374f6071dd
Gianluca Graziadei <graziadei.gianluca@gmail.com>
2023-07-21 16:44:23 +0200

    init project

---
+++ target/debug/deps/mpsg.d
@@ -0,0 +1,5 @@
+C:\Users\Gianluca Graziadei\Desktop\RustProject\target\debug\deps\mpsg.exe: src\main.rs
+
+C:\Users\Gianluca Graziadei\Desktop\RustProject\target\debug\deps\mpsg.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/mpsg.d
@@ -0,0 +1,1 @@
+C:\Users\Gianluca\ Graziadei\Desktop\RustProject\target\debug\mpsg.exe: C:\Users\Gianluca\ Graziadei\Desktop\RustProject\src\main.rs
