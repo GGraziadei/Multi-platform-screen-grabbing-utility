e2ba0f2331a608cabd551ca1007d570b58eb609a
Bernard Borio <bernard.borio@studenti.polito.it>
2023-07-28 14:32:24 +0200

    first attempt of drawing main window

--- src/main_window.rs
+++ src/main_window.rs
@@ -1,5 +1,44 @@
+use std::default::Default;
+use eframe::IntegrationInfo;
 use egui::*;
 
-pub fn simple_window(){
+#[derive(Default)]
+struct MyEguiApp {}
 
+impl MyEguiApp {
+	fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+    // Restore app state using cc.storage (requires the "persistence" feature).
+    // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+    // for e.g. egui::PaintCallback.
+    Self::default()
+  }
+}
+
+impl eframe::App for MyEguiApp {
+  fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
+		let window_size = frame.info().window_info.size;
+		let panel_frame = egui::Frame{
+			inner_margin: Default::default(),
+			outer_margin: Default::default(),
+			rounding: Default::default(),
+			shadow: Default::default(),
+			fill: Color32::GOLD,
+			stroke: Default::default(),
+		};
+
+
+
+		Window::new("left").frame(panel_frame).default_width(window_size.x*0.5).show(ctx, |ui|{
+			ui.label("left");
+		});
+		Window::new("right").frame(panel_frame).default_width(window_size.x*0.5).show(ctx, |ui|{
+			ui.label("right");
+		});
+	}
+}
+
+pub fn simple_window(){
+  let native_options = eframe::NativeOptions::default();
+  eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc)))).unwrap();
 }
