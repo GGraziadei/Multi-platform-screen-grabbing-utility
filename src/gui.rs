5166889d286fecc2bb5bd169d4f1711d7b80677f
Bernard Borio <bernard.borio@studenti.polito.it>
2023-07-29 16:23:54 +0200

    edit

--- src/gui.rs
+++ src/gui.rs
@@ -19,52 +19,7 @@ struct Content {
 
 impl eframe::App for Content {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // egui::CentralPanel::default().show(ctx, |ui| {
-        //     ui.heading("Press/Hold/Release example. Press A to test.");
-        //     if ui.button("Clear").clicked() {
-        //         self.text.clear();
-        //     }
-        //
-        //     if ui.button("Screenshot").clicked() {
-        //         let di = DisplayInfo::from_point(0,0);
-        //         let image = self.screenshot_executor.screenshot(di.unwrap(), None, None);
-        //         ImageFormatter::from(image.unwrap()).to_clipboard().unwrap();
-        //     }
-        //
-        //     ScrollArea::vertical()
-        //         .auto_shrink([false; 2])
-        //         .stick_to_bottom(true)
-        //         .show(ui, |ui| {
-        //             ui.label(&self.text);
-        //         });
-        //
-        //     if ctx.input(|i| i.key_pressed(Key::A)) {
-        //         self.text.push_str("\nPressed");
-        //     }
-        //
-        //     if ctx.input(|i| i.key_pressed(Key::B)) {
-        //         _frame.set_minimized(true);
-        //     }
-        //
-        //     if ctx.input(|i| i.key_down(Key::A)) {
-        //         self.text.push_str("\nTake screenshot");
-        //         ui.ctx().request_repaint(); // make sure we note the holding.
-        //         let mut encoders = self.encoders.lock()
-        //             .expect("Error in encoders access");
-        //         let di = DisplayInfo::from_point(0,0);
-        //         let image = self.screenshot_executor.screenshot(di.unwrap(), None, None);
-        //
-        //         encoders.push(ImageFormatter::from(image.unwrap()).save_fmt("target/ui_test".to_string(), ImageFmt::PNG));
-        //         drop(encoders);
-        //
-        //     }
-        //     if ctx.input(|i| i.key_released(Key::A)) {
-        //         self.text.push_str("\nReleased");
-        //     }
-        // });
         let window_size = _frame.info().window_info.size;
-        _frame.set_window_title("MPSGU");
-
 
         TopBottomPanel::top("top")
           .frame(Frame{fill: hex_color!("#2B2D30"), ..Default::default()})
@@ -126,7 +81,7 @@ impl eframe::App for Content {
                     ui.checkbox(&mut true, "Cattura solo la finestra attuale");
                     ui.checkbox(&mut true, "Esci dopo il salvataggio o la copia manuali");
                     ui.checkbox(&mut true, "Cattura al click");
-
+                    let painter = ui.painter();
                   }
                 );
               });
