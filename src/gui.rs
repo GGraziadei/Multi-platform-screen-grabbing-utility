008f0a6584f47d5b24bf12d90d68fb7043fda321
Bernard Borio <bernard.borio@studenti.polito.it>
2023-07-30 14:14:54 +0200

    Trying to paint image

--- src/gui.rs
+++ src/gui.rs
@@ -1,12 +1,13 @@
+use std::cmp::min;
 use std::sync::{Arc, Mutex, RwLock};
 use std::thread::{JoinHandle, spawn};
 use eframe::emath::{Align, Vec2};
 use eframe::epaint::hex_color;
-use egui::{Direction, Frame, Id, Key, LayerId, Layout, Margin, RichText, ScrollArea, SidePanel, TopBottomPanel};
+use egui::{Color32, Direction, Frame, Id, Key, LayerId, Layout, Margin, Order, pos2, Rect, RichText, ScrollArea, SidePanel, TopBottomPanel};
 use eframe::Result;
 use egui::accesskit::Role::Image;
 use egui_extras::RetainedImage;
-use screenshots::DisplayInfo;
+use screenshots::{Compression, DisplayInfo};
 use crate::configuration::{Configuration, ImageFmt};
 use crate::image_formatter::{EncoderThread, ImageFormatter};
 use crate::screenshots::{CaptureArea, ScreenshotExecutor};
@@ -59,9 +60,25 @@ impl eframe::App for Content {
                     ui.spacing_mut().item_spacing.y = 10.0;
                     if ui.button("Regione rettangolare").clicked(){};
                     if ui.button("Tutti gli schermi").clicked(){};
-                    if ui.button("Schermo attuale").clicked(){};
+                    if ui.button("Schermo attuale").clicked(){
+                      let screenshot = self.screenshot_executor.screenshot(DisplayInfo::from_point(0,0).unwrap(), None, CaptureArea::new(0,0, window_size.x as u32,window_size.y as u32)).unwrap();
+                      let imgf = screenshot.to_png(Some(Compression::Best)).unwrap();
+                      ctx.memory_mut(|mem|{
+                        mem.data.insert_temp(Id::from("screenshot"), imgf);
+                      });
+                    };
                     if ui.button("Finestra attiva").clicked(){};
                     if ui.button("Finestra sotto al cursore").clicked(){};
+                    let mut painter = ctx.layer_painter(LayerId::new(Order::Background, Id::new("image")));
+                    painter.set_clip_rect(Rect{min: pos2(0.0, 30.0 ), max: pos2(30.00, 30.0)});
+                    ctx.memory(|mem|{
+                      let image = mem.data.get_temp::<Vec<u8>>(Id::from("screenshot"));
+                      if image.is_some(){
+                        let r_image = RetainedImage::from_image_bytes("screenshot", image.unwrap().as_slice()).unwrap();
+                        painter.image(r_image.texture_id(ctx), Rect::from_min_max(pos2(0.0, 0.0), pos2(30.0, 30.0)), Rect::from_min_max(pos2(0.0, 0.0), pos2(1.0, 1.0)), Color32::WHITE);
+                        // ui.image(r_image.texture_id(ctx), Vec2::new(300.0, 300.0));
+                      }
+                    })
                   }
                 );
               });
@@ -82,12 +99,6 @@ impl eframe::App for Content {
                     ui.checkbox(&mut true, "Cattura solo la finestra attuale");
                     ui.checkbox(&mut true, "Esci dopo il salvataggio o la copia manuali");
                     ui.checkbox(&mut true, "Cattura al click");
-                    let painter = ui.painter();
-                    let screenshot = self.screenshot_executor.screenshot(DisplayInfo::from_point(0,0).unwrap(), None, CaptureArea::new(0,0, 0,0)).unwrap();
-                    let imgf = ImageFormatter::from(screenshot);
-                    ctx.memory_mut(|mem|{
-                      mem.data.insert_temp(Id::from("screenshot"), imgf);
-                    });
                   }
                 );
               });
@@ -110,7 +121,7 @@ impl GuiThread {
         drop(configuration_read);
 
         let options = eframe::NativeOptions{
-            resizable: false,
+            // resizable: false,
             follow_system_theme: true,
             initial_window_size: Some(egui::Vec2::new(640.0, 360.0)),
             ..Default::default()
