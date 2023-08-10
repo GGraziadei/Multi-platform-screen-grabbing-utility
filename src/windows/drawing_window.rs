1927fb0b962c24f3decbfeffb2df0ca818684f7f
Bernard996 <bernardborio@gmail.com>
2023-08-10 13:56:24 +0200

    Fixed remember region in windows, changed resizing icons

--- src/windows/drawing_window.rs
+++ src/windows/drawing_window.rs
@@ -1,12 +1,17 @@
 use eframe::emath::Vec2;
-use egui::{Context, pos2};
+use egui::{Context, Id, pos2};
 use crate::window::Content;
 
 impl Content{
 	pub fn drawing_window(&mut self, ctx: &Context, _frame: &mut eframe::Frame){
-        let di = _frame.info().window_info.monitor_size;
-        _frame.set_window_size(di.unwrap());
-        _frame.set_window_pos(pos2(0.0,0.0));
-        println!("{:?}", di);
+        let monitor_size = _frame.info().window_info.monitor_size.unwrap();
+        _frame.set_window_size(monitor_size);
+        match ctx.memory(|mem| mem.data.get_temp::<bool>(Id::from("first_moved"))){
+            Some(_) => {}
+            None => {
+                _frame.set_window_pos(pos2(0.0, 0.0));
+                ctx.memory_mut(|mem| mem.data.insert_temp(Id::from("first_moved"), true));
+            }
+        };
     }
 }
